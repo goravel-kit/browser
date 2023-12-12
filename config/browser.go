347aea42f514ff43690039c7ee02ef03eac5ea73
耗子 <i@haozi.net>
2023-12-12 21:30:22 +0800

    feat: 高清无码

--- config/browser.go
+++ config/browser.go
@@ -7,10 +7,14 @@ import (
 func init() {
 	config := facades.Config()
 	config.Add("browser", map[string]any{
-		// 地址
-		"url": "ws://",
+		// 管理地址
+		"manage_url": "",
 		// 跟踪模式
 		"trace": true,
+		// 无头
+		"headless": true,
+		// 自动打开开发者工具
+		"devtools": false,
 		// 忽略证书错误
 		"ignore_cert_errors": false,
 	})
